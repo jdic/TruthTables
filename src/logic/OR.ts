e1d388eb4e512ade68f79b70d36723c8544c167e
jdi <jmotadv@gmail.com>
2024-05-08 21:13:40 -0700

    [FEAT] Adaptation and structure of new folder organization

---
+++ src/logic/OR.ts
@@ -0,0 +1,26 @@
+import type { IToPDFOptions } from '../types/DPDF'
+import type { TableType } from '../types/DTable'
+import toPDF from '../utils/toPDF'
+
+const OR = (inputs: number): { table: TableType[], toPDF: (options?: IToPDFOptions) => void } =>
+{
+  const combinations = Math.pow(2, inputs)
+  const table: TableType[]  = []
+
+  for (let i = 0; i < combinations; i++)
+  {
+    const binary = (i).toString(2).padStart(inputs, '0')
+    const row: boolean[] = []
+    
+    for (const key of binary)
+      row.push(key === '1')
+
+    const output = row.reduce((acc, curr) => acc || curr, false)
+
+    table.push([...row, output])
+  }
+
+  return { table, toPDF: (options?: IToPDFOptions) => toPDF(table, inputs, options)}
+}
+
+export default OR
